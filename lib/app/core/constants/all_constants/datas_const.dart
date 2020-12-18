ded44c2e253bee98258e09517e13e3801559e638
William Gravina <60082243+gravinawilliam@users.noreply.github.com>
2020-12-17 21:13:55 -0300

    Update datas_const.dart

--- lib/app/core/constants/all_constants/datas_const.dart
+++ lib/app/core/constants/all_constants/datas_const.dart
@@ -1,5 +1,5 @@
 class DataConst {
-  static const hasuraUrl = "https://kind-monkey-42.hasura.app/v1/graphql";
+  static const hasuraUrl = ""; 
   static const continentsUrl =
       "https://disease.sh/v3/covid-19/continents?yesterday=false&twoDaysAgo=false&allowNull=true";
   static const countriesUrl =
